9d720079c031d720cfbb55e075be1b7c03a92762
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2020-04-12 12:31:40 +0530

    analysis code addeD

---
+++ analysis/analysis.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from scipy.stats import mannwhitneyu, wilcoxon, ttest_ind, normaltest, shapiro
+
+users = pd.read_csv('users.csv')
+transactions = pd.read_csv('transactions.csv')
+
+# ----------------------------------------------------------
+
+# Get click by position
+# print(transactions.groupby("position").count())
+
+# ----------------------------------------------------------
+
+# Get count of  clickbait clickbed vs non-clickbait clicked
+article_id = transactions["article_id"].to_list()
+ncb = 0
+cb = 0
+for i in article_id:
+  if i%2 == 0:
+    cb+=1
+  else:
+    ncb+=1
+
+print("Clickbaits clicked : " + str(cb))
+print("Non-Clickbaits clicked : " + str(ncb))
+
+# ------------------------------------------------------------
+
+# Get likelihood of anyone to click clickbait vs non-clickbait
+transactions['non-clickbait'] = transactions['article_id']%2
+transactions['clickbait'] = 1-transactions['non-clickbait']
+
+print(transactions)
+
+print("T-Test (clicking on clickbait, over non-clickbait) p : " + str(ttest_ind(transactions['clickbait'], transactions['non-clickbait'],equal_var=False).pvalue))
+
+# ------------------------------------------------------------
+
+# time on page
+cb_time_on_page = transactions.loc[transactions['clickbait'] == 1]['time_on_page']
+ncb_time_on_page = transactions.loc[transactions['clickbait'] == 0]['time_on_page']
+print(ncb_time_on_page)
+print("Clickbait median : " + str(cb_time_on_page.median()))
+print("Clickbait mean : " + str(cb_time_on_page.mean()))
+print("Non-Clickbait median : " + str(ncb_time_on_page.median()))
+print("Non-Clickbait mean : " + str(ncb_time_on_page.mean()))
+
+print("T-Test (time on article clickbait vs non-clickbait) p : " + str(ttest_ind(cb_time_on_page, ncb_time_on_page,equal_var=False).pvalue))
+
+# ------------------------------------------------------------
+
+# First article read
+first_read = transactions.loc[transactions['sequence'] == 1]['clickbait']
+print(first_read)
+
+# summary_data = {
+#   'user_id':,
+#   'age':final_index,
+#   '':final_cb_or_not,
+#   'real_or_madeup':final_real_made_up,
+#   'mean':mean,
+#   'median':median,
+#   'mode':mode,
+#   'std_deviation': std,
+#   'response_count':count
+# }
---
+++ analysis/readme.md
@@ -0,0 +1,45 @@
+# Participants
+
+18 participants.
+
+1. All aged between 19-23
+2. All Science
+3. 14 Male, 4 Female
+4. 16 from city, 2 from towns
+5. 17 UG, 1 PG
+  
+# Headline
+
+## Click by Position
+
+| position | count |
+| -------- | ----- |
+| 0        | 13    |
+| 1        | 12    |
+| 2        | 7     |
+| 3        | 8     |
+| 4        | 6     |
+| 5        | 8     |
+
+ <!-- --- clicks on headline by id -- clickbait and non-clickbait -->
+
+## Clicking on Clickbait vs Non-Clickbait
+
+- 63% of all headlines clicked were clickbait. (34 out of 54)
+- 27% of all headlines clicked were non-clickbait. (20 out of 54)
+- **T-Test :** Any participant is more significantly ***(p=0.006)*** more likely to click the clickbait headline. 
+*T-Test done, distributions normal.*
+
+## What was the first article read?
+
+- 72.2% participants read a clickbait headline first. (13/18)
+
+# Read Time
+
+**Clickbait articles median     : 16.79 secs**
+Clickbait articles mean       : 55.89 secs
+**Non-Clickbait articles median : 3.03  secs**
+Non-Clickbait articles mean   : 27.51 secs
+
+Read time difference not found to be statistically significant. **p = 0.24**
+*T-Test done, distributions normal.*
