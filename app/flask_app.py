4f4eb914add14db6e08fa16e55350a3e32281302
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2020-04-13 22:44:56 +0530

    removed global, shifted to flask session

--- app/flask_app.py
+++ app/flask_app.py
@@ -4,18 +4,25 @@ the attention on clickbait healdines
 '''
 
 # imports
-from flask import Flask, render_template, url_for, redirect, request, jsonify
+from flask import Flask, render_template, url_for, redirect, request, jsonify, session
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, date, timedelta
 import random , string
 import json
 import datetime
 import requests
+# import os
 
 # initializing the App and database
 app = Flask(__name__)
+SESSION_TYPE = 'filesystem'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///store.db'
 db = SQLAlchemy(app)
+
+app.config.from_object(__name__)
+Session(app)
+# app.secret_key= os.urandom(24)
 
 #-------------------------------------------------
 # model for storage of page transactions
@@ -29,15 +36,6 @@ class Transactions(db.Model):
   time_before_click = db.Column(db.String)
   time_on_page = db.Column(db.String)
   sequence = db.Column(db.Integer)
-
-# class Recall(db.Model):
-#   u_id = db.Column(db.String, primary_key=True)
-#   article_1 = db.Column(db.String)
-#   score_1 = db.Column(db.Integer)
-#   article_2 = db.Column(db.String)
-#   score_2 = db.Column(db.Integer)
-#   article_3 = db.Column(db.String)
-#   score_3 = db.Column(db.Integer)
 
 class Users(db.Model):
   timestamp = db.Column(db.String)
@@ -58,8 +56,7 @@ def generate_random_string(stringLength=10):
 
 # to generate 6 news objects
 def generate_news_objects():
-  global news_objects
-  news_objects = []
+  news = []
   choices = [0,0,0,1,1,1]
   random.shuffle(choices)
   for i in range(0,6):
@@ -70,49 +67,43 @@ def generate_news_objects():
       headline = json_data['articles'][i]['ncb_headline']
       article_id = str(i)+'1'
     paragraphs = json_data['articles'][i]['paragraphs']
-    news_objects.append({
+    news.append({
       'headline':headline,
       'paragraphs':paragraphs,
       'article_id':article_id
     })
-  random.shuffle(news_objects)
-  return
+  random.shuffle(news)
+  return news
 
 # read data json file
 with open('data.json') as file:
   json_file = file.read()
   json_data = json.loads(json_file)
 
-# read questions json file
-# with open('questions.json') as file:
-#   json_file = file.read()
-#   json_questions = json.loads(json_file)
-
-recall_items = []
-articles_visited = []
 #-------------------------------------------------
 
 # PAGE 1
 # app route : root
 @app.route('/')
 def index():
-  global sequence
-  sequence = 0
-  global u_id
-  u_id = generate_random_string(10)
+  session['articles_visited'] = []
+  session['sequence'] = 0
+  session['u_id'] = generate_random_string(10)
   return render_template('index.html')
 
 # PAGE 2
 # app route : launch
 @app.route('/launch')
 def launch():
-  generate_news_objects()
+  session['news_objects'] = generate_news_objects()
   return render_template('launch.html')
 
 # PAGE 3
 # app route : headlines
 @app.route('/headlines')
 def headlines():
+  news_objects = session.get('news_objects')
+  sequence = session.get('sequence')
   h0 = news_objects[0]['headline']
   h1 = news_objects[1]['headline']
   h2 = news_objects[2]['headline']
@@ -125,32 +116,33 @@ def headlines():
 # app route : article
 @app.route('/article')
 def article():
-  global position
-  global time_spent
-  global article_id
-  global transaction_id
-  global articles_visited
-  
+  news_objects = session.get('news_objects')
   # generate transaction id
-  transaction_id = generate_random_string(15)
+  session['transaction_id'] = generate_random_string(15)
   # position of news link on web matrix
-  position = request.args.get('position')
+  session['position'] = request.args.get('position')
   # time spent on page before clicking on news link
-  time_spent = request.args.get('time_spent')
-  news_piece = news_objects[int(position)]
-  article_id = news_piece['article_id']
+  session['time_spent'] = request.args.get('time_spent')
+  news_piece = news_objects[int(session.get('position'))]
+  session['article_id'] = news_piece['article_id']
   headline = news_piece['headline']
   paragraphs = news_piece['paragraphs']
   # add article id to visited array, for recall test
-  articles_visited.append(article_id)
+  session['articles_visited'].append(session.get('article_id'))
   return render_template('article.html', headline=headline, paragraphs=paragraphs)
 
 # PAGE 5
 # app route : log_transactions
 @app.route('/log_transaction')
 def log_transaction():
-  global sequence
-  sequence+=1
+  u_id = session.get('u_id')
+  sequence = session.get('sequence')
+  position = session.get('position')
+  time_spent = session.get('time_spent')
+  article_id = session.get('article_id')
+  transaction_id = session.get('transaction_id')
+  session['sequence'] = sequence + 1
+  sequence = sequence = session.get('sequence')
   ts = datetime.datetime.now().timestamp()
   read_time = request.args.get('read_time')
   ip = request.remote_addr
@@ -158,84 +150,12 @@ def log_transaction():
   position=position,time_before_click=time_spent,time_on_page=read_time, sequence=sequence)
   db.session.add(new_transaction)
   db.session.commit()
-  print(sequence)
   if sequence == 3:
     sequence = 0
     # return redirect('/recall_test')
     return redirect('/details')
   else:
     return redirect('/headlines')
-
-#---------------------------------------
-
-#########################################
-# CODE NOT BEING USED STARTING FROM HERE
-#########################################
-
-# # app route : recall_test
-# @app.route('/recall_test')
-# def recall_test():
-
-#   global articles_visited
-#   global questions
-#   global recall_items
-
-#   questions = []
-#   questions_list = []
-
-#   for i in articles_visited:
-#     if i == '00' or i == '01':
-#       recall_items.append('train')
-#     elif i == '10' or i == '11':
-#       recall_items.append('law')
-#     elif i == '20' or i == '21':
-#       recall_items.append('justice')
-#     elif i == '30' or i == '31':
-#       recall_items.append('apple')
-#     elif i == '40' or i == '41':
-#       recall_items.append('volcano')
-#     elif i == '50' or i == '51':
-#       recall_items.append('delhi')
-  
-#   for i in recall_items:
-#     questions_list.append({'id': i, 'questions': json_questions[i]})
-#   for i in questions_list:
-#     for j in range(0,4):
-#       questions.append({'id':i['id']+str(j),'question':i['questions'][j]['question'],\
-#        'options':i['questions'][j]['options'],'correct':i['questions'][j]['correct']})
-#   random.shuffle(questions)
-#   for i in questions:
-#     random.shuffle(i['options'])
-#   return render_template('recall.html', questions=questions, articles=recall_items)
-
-
-# # save recall data to log
-# @app.route('/save_to_log')
-# def save_to_log():
-#   global u_id
-#   incoming_ids = []
-#   score = [0 for i in range(0,len(recall_items))]
-#   for i in recall_items:
-#     for j in range(0,4):
-#       incoming_ids.append(i+str(j))
-#   responses = []
-#   for i in incoming_ids:
-#     responses.append(request.args.get(i))
-#   for i in range (len(recall_items)):
-#     for j in range (len(incoming_ids)):
-#       if recall_items[i] in incoming_ids[j] and responses[j] == '0':
-#         score[i]+=1
-
-#   new_recall = Recall(u_id=u_id,article_1=recall_items[0],\
-#   score_1=score[0],article_2=recall_items[1],score_2=score[1],\
-#    article_3 = recall_items[2], score_3 = score[2])
-#   db.session.add(new_recall)
-#   db.session.commit()
-#   return redirect('/details')
-
-#########################################
-# CODE NOT BEING USED UP UNTIL HERE
-#########################################
 
 # app route : end
 @app.route('/end')
@@ -249,7 +169,7 @@ def details():
 # save demographic form data submission
 @app.route('/form_data', methods=['GET', 'POST'])
 def form_data():
-  global u_id
+  u_id = session.get('u_id')
   age = request.args.get('age')
   gender = request.args.get('gender')
   residence = request.args.get('residence')
@@ -261,11 +181,9 @@ def form_data():
   new_user = Users(timestamp=ts,u_id=u_id,age=age,gender=gender,residence=residence, edu_level=edu_level, edu_stream=edu_stream,news_source=news_source, news_interest=news_interest)
   db.session.add(new_user)
   db.session.commit()
-
   return redirect('/end')
 
 # ---------------------------------------
-
 
 if __name__ == "__main__":
   app.run(debug=True)
