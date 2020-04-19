04918dee5ce8fa9d00c75b45c221040911ec1bd7
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2020-04-19 22:49:25 +0530

    masscomm vs science analysis done

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -2,59 +2,53 @@ import pandas as pd
 from scipy.stats import mannwhitneyu, wilcoxon, ttest_ind, normaltest, shapiro
 
 users = pd.read_csv('data/users.csv')
+users['id'] = users['u_id']
+users = users.set_index('u_id')
 transactions = pd.read_csv('data/transactions.csv')
+transactions = transactions.set_index('u_id')
 
 # column to find out whether clickbait or non-clicbait clicked
 transactions['non-clickbait'] = transactions['article_id']%2
 transactions['clickbait'] = 1-transactions['non-clickbait']
 
-# ----------------------------------------------------------
+joined_table = transactions.join(users, how="left", lsuffix="_trans", rsuffix="_user")
+science = joined_table.loc[joined_table['edu_stream']=="Science"]
+mass_comm = joined_table.loc[joined_table['edu_stream']=="MassComm"]
 
-# Get click by position
-output = transactions.groupby("position").count()
-print ("Clicks by position on page : ")
-print(output['article_id'])
 
-# ----------------------------------------------------------
+def print_analysis_summary(table):
+  # Get click by position
+  output = table.groupby("position").count()
+  print ("Clicks by position on page : ")
+  print(output['article_id'])
+  
+  # Get count of  clickbait clickbed vs non-clickbait clicked
+  article_id = table["article_id"].to_list()
+  ncb = 0
+  cb = 0
+  for i in article_id:
+    if i%2 == 0:
+      cb+=1
+    else:
+      ncb+=1
+  print("Total Clickbaits clicked : " + str(cb))
+  print("Total Non-Clickbaits clicked : " + str(ncb))
 
-# Get count of  clickbait clickbed vs non-clickbait clicked
-article_id = transactions["article_id"].to_list()
-ncb = 0
-cb = 0
-for i in article_id:
-  if i%2 == 0:
-    cb+=1
-  else:
-    ncb+=1
+  # find out likelihood of clickbait being clicked over non-clickbait
+  print("T-Test (clicking on clickbait, over non-clickbait) p : " + str(ttest_ind(table['clickbait'], table['non-clickbait'],equal_var=False).pvalue))
+  # print("Wilcoxon (clicking on clickbait, over non-clickbait) p : " + str(wilcoxon(table['clickbait'], table['non-clickbait']).pvalue))
 
-print("Total Clickbaits clicked : " + str(cb))
-print("Total Non-Clickbaits clicked : " + str(ncb))
+  # # First article read
+  first_read = table.loc[table['sequence'] == 1]['clickbait']
+  print("First Article read counts : ")
+  print("Clickbait count : " + str(first_read.tolist().count(1)))
+  print("Non-Clickbait count : " + str(first_read.tolist().count(0)))
 
-# # ------------------------------------------------------------
-# find out likelihood of clickbait being clicked over non-clickbait
-# # print(transactions)
-print("T-Test (clicking on clickbait, over non-clickbait) p : " + str(ttest_ind(transactions['clickbait'], transactions['non-clickbait'],equal_var=False).pvalue))
+print("\n ALL :\n")
+print_analysis_summary(transactions)
+print("\n SCIENCE :\n")
+print_analysis_summary(science)
+print("\n MASS COMM :\n")
+print_analysis_summary(mass_comm)
 
-# # ------------------------------------------------------------
 
-# time on page
-# cb_time_on_page = transactions.loc[transactions['clickbait'] == 1]['time_on_page']
-# ncb_time_on_page = transactions.loc[transactions['clickbait'] == 0]['time_on_page']
-# print(ncb_time_on_page)
-# print("Clickbait median : " + str(cb_time_on_page.median()))
-# print("Clickbait mean : " + str(cb_time_on_page.mean()))
-# print("Non-Clickbait median : " + str(ncb_time_on_page.median()))
-# print("Non-Clickbait mean : " + str(ncb_time_on_page.mean()))
-
-# print("T-Test (time on article clickbait vs non-clickbait) p : " + str(ttest_ind(cb_time_on_page, ncb_time_on_page,equal_var=False).pvalue))
-
-# print(normaltest(cb_time_on_page).pvalue)
-# print(normaltest(ncb_time_on_page).pvalue)
-
-# # # ------------------------------------------------------------
-
-# # First article read
-first_read = transactions.loc[transactions['sequence'] == 1]['clickbait']
-print("First Article read counts : ")
-print("Clickbait count : " + str(first_read.tolist().count(1)))
-print("Non-Clickbait count : " + str(first_read.tolist().count(0)))
